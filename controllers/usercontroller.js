07622e7f0f91762f69eeaf12facd300a9d7e1d80
AnnaDavydenko <annadavydenko2544737@gmail.com>
2021-05-23 22:51:29 +0300

    fix: models, sequelize mistake. Update package.json

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../db').User;
 
@@ -7,12 +7,12 @@ router.post('/signup', (req, res) => {
     User.create({
         full_name: req.body.user.full_name,
         username: req.body.user.username,
-        passwordhash: bcrypt.hashSync(req.body.user.password, 10),
+        passwordHash: bcrypt.hashSync(req.body.user.password, 10),
         email: req.body.user.email,
     })
         .then(
             function signupSuccess(user) {
-                let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                const token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
                 res.status(200).json({
                     user: user,
                     token: token
@@ -22,15 +22,15 @@ router.post('/signup', (req, res) => {
             function signupFail(err) {
                 res.status(500).send(err.message)
             }
-        )
-})
+        );
+});
 
 router.post('/signin', (req, res) => {
     User.findOne({ where: { username: req.body.user.username } }).then(user => {
         if (user) {
             bcrypt.compare(req.body.user.password, user.passwordHash, function (err, matches) {
                 if (matches) {
-                    var token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                    const token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
                     res.json({
                         user: user,
                         message: "Successfully authenticated.",
@@ -45,6 +45,6 @@ router.post('/signin', (req, res) => {
         }
 
     })
-})
+});
 
 module.exports = router;
--- db.js
+++ db.js
@@ -8,15 +8,13 @@ const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env
     dialect: 'postgres'
 });
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
-
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-);
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
 
 module.exports = {
     sequelize,
